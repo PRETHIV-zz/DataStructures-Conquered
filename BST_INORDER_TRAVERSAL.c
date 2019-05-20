ac3282bd763d643c45c5c7a904cb330a405ac94a
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-05-20 13:57:45 +0530

    Create BST_INORDER_TRAVERSAL.c

---
+++ BST_INORDER_TRAVERSAL.c
@@ -0,0 +1,77 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node{
+    int data;
+    struct Node *left,*right;
+
+};
+
+struct Node* newnode(int data)
+{
+    struct Node *nn=(struct Node*)malloc(sizeof(struct Node));
+    nn->data=data;
+    nn->left=NULL;
+    nn->right=NULL;
+    return nn;
+}
+
+
+struct Node* insert(struct Node* root,int key)
+{
+ if(root==NULL){
+    return newnode(key);
+ }
+ else{
+    if(key<=root->data)
+    {
+        root->left=insert(root->left,key);
+    }
+    else{
+        root->right=insert(root->right,key);
+    }
+    return root;
+ }
+}
+
+
+ void inorder(struct Node* root)
+ {
+     if(root==NULL){
+        return;
+     }
+     else{
+        inorder(root->left);
+        printf("%d ",root->data);
+        inorder(root->right);
+     }
+ }
+
+
+ int main()
+ {
+     int n;
+     printf("\n\nEnter no of nodes u want to enter");
+     scanf("%d",&n);
+     struct Node *root=NULL;
+     int c=0;
+     while(n>0)
+     {
+         int key;
+         scanf("%d",&key);
+         if(c==0){
+            c++;
+            root=insert(root,key);
+         }
+         else{
+            insert(root,key);
+         }
+
+         n--;
+     }
+
+     inorder(root);
+     return 0;
+ }
+
+
