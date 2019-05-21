ef2dfa4682f1ac922108cadef3975a21b2919022
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-05-21 12:01:45 +0530

    Update BST_INORDER_TRAVERSAL.c

--- BST_INORDER_TRAVERSAL.c
+++ BST_INORDER_TRAVERSAL.c
@@ -1,23 +1,34 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-struct Node{
+//Hi i am Prethiv "THE SO CALLED C PROGRAMMER"
+
+struct Node{       /*Specifying node as a structure*/
     int data;
     struct Node *left,*right;
 
 };
 
-struct Node* newnode(int data)
+struct Node* newnode(int data)   /*A handwritten function to create a new node.The function takes one argument ,the data value of node to be created*/
 {
-    struct Node *nn=(struct Node*)malloc(sizeof(struct Node));
-    nn->data=data;
-    nn->left=NULL;
-    nn->right=NULL;
-    return nn;
+    struct Node *nn=(struct Node*)malloc(sizeof(struct Node));  /*Creating a newnode using malloc*/
+    nn->data=data;//Newnode's data equals to passed argument
+    nn->left=NULL;//Nenode's left is nULL
+    nn->right=NULL;//Newnode's right is null
+    return nn;//return the created newnode
 }
 
+/*The insert function is used to insert  new node in the tree
+    if the root is null then create a newnode for the data given and assign it the root
+    else:
+        if the key given is less than the root then call the function again by assigning root->left and passing root->left instead of root
+      
+        if the key is greater than the root then call the same function again by asigning root->right and passing root->right instead of root
 
-struct Node* insert(struct Node* root,int key)
+*/
+
+
+struct Node* insert(struct Node* root,int key)  
 {
  if(root==NULL){
     return newnode(key);
@@ -34,6 +45,21 @@ struct Node* insert(struct Node* root,int key)
  }
 }
 
+
+
+/*The inorder traversal of a given tree 
+    As it is straight forward logic i think no need to explain this for more info about inorder traversal visit geeksforgeeks
+    
+    
+            90
+           /  \
+          86   91
+         / \   /  \ 
+        85 88 null 93
+        
+        The inorder traversal of the given tree is : 85 86 88 90 91 93
+        The level order traversal of the given tree is :90 86 91 85 88 93
+ */
 
  void inorder(struct Node* root)
  {
